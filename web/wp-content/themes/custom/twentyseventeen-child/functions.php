eeca0bd61387ddf207564ea142202ded6c61b703
Rachel Whitton <rachel@getpantheon.com>
2017-10-25 11:30:21 -0500

    Require lh-hsts plugin, test custom function to add header

--- web/wp-content/themes/custom/twentyseventeen-child/functions.php
+++ web/wp-content/themes/custom/twentyseventeen-child/functions.php
@@ -10,3 +10,13 @@ function twentyseventeen_parent_theme_enqueue_styles() {
     );
 
 }
+
+/**
+*
+* Example custom function to add the HSTS header while rendering a response.
+*
+**/
+add_action( 'send_headers', 'add_header_hsts' );
+function add_header_hsts() {
+    header('Strict-Transport-Security: max-age=15984000; includeSubDomains; preload');
+}
