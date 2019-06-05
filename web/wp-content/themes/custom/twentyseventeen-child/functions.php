cd77ee008bcc6271fb58f75fa667eeab02429d96
Rachel Whitton <rachel@getpantheon.com>
2019-06-05 13:06:18 -0500

    Fix build failure, run auto fix for phpcs

--- web/wp-content/themes/custom/twentyseventeen-child/functions.php
+++ web/wp-content/themes/custom/twentyseventeen-child/functions.php
@@ -1,10 +1,12 @@
 <?php
 
-add_action( 'wp_enqueue_scripts', 'twentyseventeen_parent_theme_enqueue_styles' );
+add_action('wp_enqueue_scripts', 'twentyseventeen_parent_theme_enqueue_styles');
 
-function twentyseventeen_parent_theme_enqueue_styles() {
-    wp_enqueue_style( 'twentyseventeen-style', get_template_directory_uri() . '/style.css' );
-    wp_enqueue_style( 'twentyseventeen-child-style',
+function twentyseventeen_parent_theme_enqueue_styles()
+{
+    wp_enqueue_style('twentyseventeen-style', get_template_directory_uri() . '/style.css');
+    wp_enqueue_style(
+        'twentyseventeen-child-style',
         get_stylesheet_directory_uri() . '/style.css',
         array( 'twentyseventeen-style' )
     );
@@ -12,11 +14,10 @@ function twentyseventeen_parent_theme_enqueue_styles() {
 }
 
 /**
-*
 * Example custom function to add the HSTS header while rendering a response.
-*
 **/
-add_action( 'send_headers', 'add_header_hsts' );
-function add_header_hsts() {
+add_action('send_headers', 'add_header_hsts');
+function add_header_hsts()
+{
     header('Strict-Transport-Security: max-age=15984000; includeSubDomains; preload');
 }
